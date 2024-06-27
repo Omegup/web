89ebfccc659a78df82daf46e9279ef68c9ede5dc
Youssef <youssefmosbah04@gmail.com>
2024-06-27 10:32:07 +0100

    first work

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function NavBar() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    
+    <div className='container1'>
+      
+      <div className='affiche'>
+      <span className='nav1'>Omegup</span>
+      
+      <a href='#' className='nav2'>Home</a>
+      <a href='#' className='nav2'>About Us</a>
+      <a href='#' className='nav2'>Services</a>
+      <a href='#' className='nav2'>References</a>
+      <a href='#' id='button'>Get in Touch</a>
+    </div>
+    <div className='texte'>
+      <h1>Welcome to <span id='titre1'>Omegup</span> :<br/>Where <span id='titre2'>Imagination</span> meets <span id='titre3'>Code </span>&lt; / &gt;</h1>
+    </div>
+    </div>
   )
+
 }
 
-export default App
+
+
+
+export default NavBar
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import NavBar from './App'
+import Acceuil from './App2'
+import Head from './App3'
+import Services from './Services'
+import Footer from './Footer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <Acceuil />
+    <Head />
+    <Services />
+    <Footer />
   </React.StrictMode>,
 )
